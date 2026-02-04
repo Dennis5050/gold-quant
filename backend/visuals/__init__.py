62522d7f1a0631119a6ccd4c081017becad10666
Deno5050 <deniskiplagat204@gmail.com>
2026-02-04 22:48:03 +1200

    visualsinit

---
+++ backend/visuals/__init__.py
@@ -0,0 +1,12 @@
+# backend/visuals/__init__.py
+
+"""
+Visualizations package for Gold-Quant system.
+
+Modules:
+- zones: Draws SL/TP zones on price charts
+- equity_curve: Plots account equity/performance curves
+"""
+
+from . import zones
+from . import equity_curve
